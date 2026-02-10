5348d6244c33eeb0757e74f9081a0a11f717e5d1
Amal_Halim <amalhalim333@gamil.com>
2026-02-10 22:02:30 +0200

    feat: Add contact page with dedicated section and establish global styling with Tailwind CSS, dark mode support, and custom utilities.

---
+++ atheer/src/app/(pages)/user/[slug_title]/components/ContactSection.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function ContactSection() {
+    return (
+        <section id="contact" className="mb-24 py-12 text-center">
+            <h2 className="text-3xl font-bold text-slate-200 mb-6">Get In Touch</h2>
+            <p className="text-slate-400 mb-8 max-w-lgmx-auto ">
+                Although I&apos;m not currently looking for any new opportunities, my inbox is always open.
+                Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
+            </p>
+            <a
+                href="mailto:hello@example.com"
+                className=" px-8 py-4 border border-cyan-400 text-cyan-400  rounded hover:bg-cyan-400/10 "
+            >
+                Say Hello
+            </a>
+        </section>
+    )
+}
