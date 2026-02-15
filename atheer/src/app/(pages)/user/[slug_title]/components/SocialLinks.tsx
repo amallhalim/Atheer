fd70030af15b748db740eb2a607270a97967050e
Amal_Halim <amalhalim333@gamil.com>
2026-02-15 10:18:18 +0200

    feat: create SocialLinks component to display social media icons for user profiles.

--- atheer/src/app/(pages)/user/[slug_title]/components/SocialLinks.tsx
+++ atheer/src/app/(pages)/user/[slug_title]/components/SocialLinks.tsx
@@ -4,19 +4,20 @@ import { SiFacebook, SiInstagram, SiLinkedin, SiX } from "react-icons/si";
 
 export default function SocialLinks() {
     const socialLinks = [
-        { name: "X", url: "https://twitter.com/", icon: <SiX className="w-4 h-4" /> },
-        { name: "Facebook", url: "https://facebook.com/", icon: <SiFacebook className="w-4 h-4" /> },
-        { name: "Instagram", url: "https://instagram.com/", icon: <SiInstagram className="w-4 h-4" /> },
-        { name: "Linkedin", url: "https://linkedin.com/", icon: <SiLinkedin className="w-4 h-4" /> },
+        { name: "X", url: "https://twitter.com/", icon: <SiX className="w-6 h-6" /> },
+        { name: "Facebook", url: "https://facebook.com/", icon: <SiFacebook className="w-6 h-6" /> },
+        { name: "Instagram", url: "https://instagram.com/", icon: <SiInstagram className="w-6 h-6" /> },
+        { name: "Linkedin", url: "https://linkedin.com/", icon: <SiLinkedin className="w-6 h-6" /> },
     ]
     return (
-        <ul className='flex flex-row space-x-4 items-center'>
+        <ul className='flex flex-row space-x-6 items-center'>
             {socialLinks.map((link) => (
                 <li
                     key={link.name}
                     onClick={() => window.open(link.url, '_blank')}
                     className="link hover:scale-120"
                     title={link.name}
+                    aria-label={link.name}
                 >
                     {link.icon}
                 </li>
