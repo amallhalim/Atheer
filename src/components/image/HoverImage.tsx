ac11c4a0e168de26a9f6537a08718c1518a0dc9a
Amal_Halim <amalhalim333@gamil.com>
2026-02-18 09:02:22 +0200

    feat: add HoverImage component for displaying images with a hover effect

--- src/components/image/HoverImage.tsx
+++ src/components/image/HoverImage.tsx
@@ -4,18 +4,16 @@ import Image, { StaticImageData } from 'next/image'
 
 export default function HoverImage({ url, title }: { url: StaticImageData | string, title: string }) {
     return (
-        <div className="relative  overflow-hidden rounded-xl border border-slate-700/50 shadow-xl  ">
+        <div className="relative overflow-hidden rounded-xl border border-slate-700/50 shadow-xl aspect-video">
             <Image
                 src={url}
                 alt={title}
                 fill
-                className="object-cover transition-transform duration-700
-                 group-hover:scale-105 
-                  bg-slate-950/20 group-hover:bg-transparent 
-                 "
+                className="object-cover transition-transform duration-700 group-hover:scale-105 bg-slate-950/20 group-hover:bg-transparent"
+                quality={75}
             />
             {/* Glassy overlay */}
-            <div className="absolute  bg-slate-950/20 group-hover:bg-transparent " />
+            <div className="absolute inset-0 bg-slate-950/20 group-hover:bg-transparent transition-colors duration-500" />
         </div>
     )
 }
